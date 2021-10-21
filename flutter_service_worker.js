13b2cd60a1fa2d452a86c025b7ff1ed4ad059f95
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-10-21 08:52:18 -0700

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "00ad7bb8baa65896d10b8e1535f8c561",
+  "main.dart.js": "f992248525ce6065c35bb915084ea673",
 "manifest.json": "010f129b3b174dde9a468793a6ccb6c2",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "c65f29d5b3881286d36cac8ce5521b64",
@@ -207,8 +207,8 @@ const RESOURCES = {
 "assets/packages/flutter_inappwebview/t_rex_runner/t-rex.html": "16911fcc170c8af1c5457940bd0bf055",
 "assets/packages/flutter_inappwebview/t_rex_runner/t-rex.css": "5a8d0222407e388155d7d1395a75d5b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "f9e07356261330c392f626c84fdfa3bb",
-"/": "f9e07356261330c392f626c84fdfa3bb",
+"index.html": "e4cd2b5a5282cf83d2ac9c3e4ff67acc",
+"/": "e4cd2b5a5282cf83d2ac9c3e4ff67acc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
