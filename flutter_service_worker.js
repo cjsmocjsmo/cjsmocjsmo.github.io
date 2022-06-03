071e28524558d1d2f259fe1c336bf720a50eb830
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-06-03 12:12:29 -0700

    nwe test version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "810ec5a2fb2ae752e91ddb785de3acf0",
-"/": "810ec5a2fb2ae752e91ddb785de3acf0",
+  "index.html": "db40eb94a1d833772e118092747cc4b8",
+"/": "db40eb94a1d833772e118092747cc4b8",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "assets/images/banner.webp": "257221fa0e9f985cf12504522a4019de",
 "assets/images/curt.webp": "43a7b06a5b4e679d537bb7015460e35d",
@@ -33,7 +33,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "version.json": "3320c9fee45edf29895ca3c2a87ab303",
-"main.dart.js": "6a8f56ae21577705830fbed3fd173d26"
+"main.dart.js": "e00ec4ca2b44cf2c4cc24ae29828523f"
 };
 
 // The application shell files that are downloaded before a service worker can
